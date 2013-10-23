31b2d14a34f8621bc8879f2bfaa471d28d773b57
Karina1988 <karina.ennenga@gmail.com>
2013-10-23 15:55:31 +0200

    added Frage

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,5 +3,7 @@ import java.util.*;
  class Studienberatung{
 	public static void main(String [] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+		String antwort = scanner.next();
 	}
 }
